b0249c4eb04fdab6119c6ab813cf27149188c229
lubaochuan <lubaochuan@gmail.com>
2017-03-07 15:51:12 -0600

    add ScrollView in a container with fixed height

---
+++ MyPresentationalComponent.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+
+export default MyPresentationalComponent = (props) => {
+  const createItem = (item) => (
+    <Text key={item.id} style={styles.item}>
+      {item.name}
+    </Text>
+  )
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {props.listItems.map(createItem)}
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create ({
+  container: {
+    marginTop: 50,
+    height: 500,
+    backgroundColor: 'silver'
+  },
+  item: {
+    margin: 15,
+    padding: 15,
+    height: 40,
+    borderColor: 'red',
+    borderWidth: 1
+  }
+})
---
+++ index.ios.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { View, AppRegistry } from 'react-native'
+import MyPresentationalComponent from './MyPresentationalComponent'
+
+export default class MyApp extends Component {
+  constructor() {
+    super()
+    this.state = {
+     listItems: [
+       {'name': 'Item1', 'id': 1},
+       {'name': 'Item2', 'id': 2},
+       {'name': 'Item3', 'id': 3},
+       {'name': 'Item4', 'id': 4},
+       {'name': 'Item5', 'id': 5},
+       {'name': 'Item6', 'id': 6},
+       {'name': 'Item7', 'id': 7},
+       {'name': 'Item8', 'id': 8},
+       {'name': 'Item9', 'id': 9},
+       {'name': 'Item10', 'id': 10},
+       {'name': 'Item11', 'id': 11},
+       {'name': 'Item12', 'id': 12}]
+     }
+  }
+
+  render(){
+    return (
+      <View>
+        <MyPresentationalComponent listItems = {this.state.listItems}/>
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('RN_ScrollView', () => MyApp);
